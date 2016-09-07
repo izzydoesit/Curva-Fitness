c2eb07b8c2cec1cababe9e1ac4a82c2233891a26
Izzy Matos <israeldmatos@gmail.com>
2016-09-06 23:27:44 -0700

    Adds rails namespace back to production env config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-CurvaProto2::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
