ac10c91bb4c00754b1ebc7209e421b0e2b90ed47
Izzy Matos <israeldmatos@gmail.com>
2016-09-06 23:26:08 -0700

    Deletes CurvaProto2 module

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CurvaProto2
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-      config.assets.initialize_on_precompile = false
-
+    config.assets.initialize_on_precompile = false
   end
-end
