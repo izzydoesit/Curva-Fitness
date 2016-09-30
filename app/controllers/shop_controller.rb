dd9ea3d8b7417c116be84fba92b73e78a39fc023
Izzy Matos <israeldmatos@gmail.com>
2016-09-30 02:50:48 -0700

    Adds shop controller with shopping cart actions

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,4 +1,31 @@
 class ShopController < ApplicationController
+  def add
+    id = params[:id]
+    if session[:cart]
+      cart = session[:cart]
+    else
+      session[:cart] = {}
+      cart = session[:cart]
+    end
+
+    if cart[id]
+      cart[id] = cart[id] + 1
+    else
+      cart[id] = 1
+    end
+    redirect_to action: :index
+  end
+
+  def clearCart
+    session[:cart] = nil
+    redirect_to action: :index
+  end
+
   def index
+    if session[:cart]
+      @cart = session[:cart]
+    else
+      @cart = {}
+    end
   end
 end
