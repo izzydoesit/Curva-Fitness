972dedc340d207cfd223fb76b692417bc4811df9
Izzy Matos <israeldmatos@gmail.com>
2016-09-30 05:04:56 -0700

    Fixes authenticate user method reference

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,6 +1,6 @@
 class ShopController < ApplicationController
 
-  before_action authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index]
   
   def add
     id = params[:id]
