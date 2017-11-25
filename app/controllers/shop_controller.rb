229478dd7f596d41de7ceca34921e3cb79ad442c
everysum1 <israeldmatos@gmail.com>
2017-11-24 23:06:29 -0800

    Delete cart methods from shop controller, add index action w/ all products var

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,34 +1,6 @@
 class ShopController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index]
-  
-  def add
-    id = params[:id]
-    if session[:cart]
-      cart = session[:cart]
-    else
-      session[:cart] = {}
-      cart = session[:cart]
-    end
-
-    if cart[id]
-      cart[id] = cart[id] + 1
-    else
-      cart[id] = 1
-    end
-    redirect_to action: :index
-  end
-
-  def clearCart
-    session[:cart] = nil
-    redirect_to action: :index
-  end
-
   def index
-    if session[:cart]
-      @cart = session[:cart]
-    else
-      @cart = {}
-    end
+    @products = Product.all 
   end
 end
