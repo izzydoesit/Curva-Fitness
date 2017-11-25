f6cf9c90a9ec9d54ab0b9745be71ab6d08792bc5
everysum1 <israeldmatos@gmail.com>
2017-11-24 23:05:27 -0800

    Add functionality to cart controller actions

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,13 +1,37 @@
 class CartController < ApplicationController
+
+  before_action :authenticate_user!
+  
   def add
+    id = params[:id]
+    if session[:cart]
+      cart = session[:cart]
+    else
+      session[:cart] = {}
+      cart = session[:cart]
+    end
+
+    if cart[id]
+      cart[id] = cart[id] + 1
+    else
+      cart[id] = 1
+    end
+    redirect_to action: :index
   end
 
   def remove
   end
 
   def clearCart
+    session[:cart] = nil
+    redirect_to action: :index
   end
 
   def index
+    if session[:cart]
+      @cart = session[:cart]
+    else
+      @cart = {}
+    end
   end
 end
