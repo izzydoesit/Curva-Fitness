0e4e25e978e240b021a49f4750e293f449469e13
everysum1 <israeldmatos@gmail.com>
2017-11-26 04:02:36 -0800

    Update products table migration w/ active field

--- db/migrate/20160930080724_create_products.rb
+++ db/migrate/20160930080724_create_products.rb
@@ -1,6 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
+      t.boolean :active, default: true
       t.string :brand, null: false
       t.string :name, null: false
       t.text :description, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -42,14 +42,15 @@ ActiveRecord::Schema.define(version: 20171126105249) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.string   "brand",       null: false
-    t.string   "name",        null: false
-    t.text     "description", null: false
+    t.boolean  "active",      default: true
+    t.string   "brand",                      null: false
+    t.string   "name",                       null: false
+    t.text     "description",                null: false
     t.string   "image_url"
-    t.decimal  "price",       null: false
+    t.decimal  "price",                      null: false
     t.string   "category"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
