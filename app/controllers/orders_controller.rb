ab9d6ba1c2ad349ad4334ce6314a76f6a1ecafc9
everysum1 <israeldmatos@gmail.com>
2017-11-26 22:41:30 -0800

    Add charge_card action to orders method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,45 @@
 class OrdersController < ApplicationController
 
+  def charge_card
+    current_order.calculate_total
+
+    SquareConnect.configure do |config|
+      # Configure OAuth2 access token for authorization: oauth2
+      config.access_token = ENV['SQUARE_ACCESS_TOKEN']
+    end
+    transactions_api = SquareConnect::TransactionsApi.new
+    
+    amount = current_order.total.to_i * 100
+    request_body = {
+      card_nonce: params["nonce"],
+      amount_money: {
+        amount: amount,
+        currency: 'USD'
+      },
+      idempotency_key: SecureRandom.uuid
+    }
+    location_id = ENV['SQUARE_LOCATION_ID']
+    begin
+      resp = transactions_api.charge(location_id, request_body)
+    rescue SquareConnect::ApiError => e
+      Rails.logger.error("Error encountered while charging card:: #{e.message}")
+      render json: {status: 400, errors: JSON.parse(e.response_body)["errors"]}
+      return
+    end
+    puts resp
+    transaction_id = resp[:transaction][:tenders].first[:transaction_id]
+    charge_time = resp[:transaction][:created_at]
+    location_id = resp[:transaction][:location_id]
+    current_order.update_columns(
+      transaction_id: transaction_id, 
+      charged_at: charge_time, 
+      location_id: location_id,
+      order_status_id: 2
+    )
+
+    redirect_to :root
+  end
+
   def update
     @order.update(order_params)
     if @order.save
@@ -14,6 +54,6 @@ class OrdersController < ApplicationController
 
   private
     def order_params
-    params.require(:order).permit(:first_name, :last_name, :email, :address1, :address2, :city, :state, :zip_code, )
+    params.require(:order).permit(:first_name, :last_name, :email, :address1, :address2, :city, :state, :zip_code, :charged)
     end
 end
