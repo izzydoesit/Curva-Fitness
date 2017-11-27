c6cd3cf0e073b18655550fe674e55a3681a0bd84
everysum1 <israeldmatos@gmail.com>
2017-11-26 17:10:19 -0800

    Fix model callback to execute ealier in lifecycle

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :order_status
   has_many :order_items
-  before_create :set_order_status
+  before_validation :set_order_status
   before_save :update_subtotal
 
   def subtotal
