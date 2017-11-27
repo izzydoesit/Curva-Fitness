09cc1a94ca9000b07a476574a27f428c3696a498
everysum1 <israeldmatos@gmail.com>
2017-11-26 20:18:20 -0800

    Add update method for Orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,19 @@
 class OrdersController < ApplicationController
-  def create
-  end
 
   def update
+    @order.update(order_params)
+    if @order.save
+      redirect_to cart_path(@order)
+    else
+      render "carts/show"
+    end
   end
 
   def destroy
   end
+
+  private
+    def order_params
+    params.require(:order).permit(:first_name, :last_name, :email, :address1, :address2, :city, :state, :zip_code, )
+    end
 end
