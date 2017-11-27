6944e2492ea40f37fba50fcff14b628daf81bd5f
everysum1 <israeldmatos@gmail.com>
2017-11-26 16:58:27 -0800

    Add current_order helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_order
 
-  def hello
-    render html: "hello world!"
+  def current_order
+    if !session[:order_id].nil?
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
   end
 end
