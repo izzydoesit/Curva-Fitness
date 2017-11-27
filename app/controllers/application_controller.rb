9e12df48aaabf62d9aff045620992dffaf35feaa
everysum1 <israeldmatos@gmail.com>
2017-11-26 19:28:03 -0800

    Add helper method to display total quantity in header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_order
+  helper_method :current_order, :current_quantity
 
   def current_order
     if !session[:order_id].nil?
       Order.find(session[:order_id])
     else
       Order.new
+    end
+  end
+
+  def current_quantity
+    if !session[:order_id].nil?
+      order = Order.find(session[:order_id])
+      order.order_items.inject(0) { |qty, item| qty + item.quantity }
+    else
+      0
     end
   end
 end
