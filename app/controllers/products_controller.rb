957bf1684de1784eb2d0f9c08ec4c1afff5bf67d
everysum1 <israeldmatos@gmail.com>
2017-11-26 18:58:09 -0800

    Add order_item var to view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    @order_item = current_order.order_items.new
   end
 
   # GET /products/1
